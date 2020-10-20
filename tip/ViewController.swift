7ce6a99db3a04c71893ffa550b96c80606c8af34
Nayoung Park <36286435+nap015@users.noreply.github.com>
2020-10-19 17:42:38 -0700

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Nayoung on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
